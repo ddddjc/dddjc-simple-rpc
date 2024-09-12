50f8b941b2a8f887bf4ea3720195abcd8938e601
du jiachen <930657303@qq.com>
2024-09-12 19:43:35 +0800

    proxy

--- rpc-framework-common/src/main/java/com/rpc/proxy/RpcClientProxy.java
+++ rpc-framework-common/src/main/java/com/rpc/proxy/RpcClientProxy.java
@@ -1,9 +1,21 @@
 package com.rpc.proxy;
 
+import com.rpc.config.RpcServiceConfig;
+import com.rpc.enums.RpcErrorMessageEnum;
+import com.rpc.enums.RpcResponseCodeEnum;
+import com.rpc.exception.RpcException;
+import com.rpc.remoting.dto.RpcRequest;
+import com.rpc.remoting.dto.RpcResponse;
+import com.rpc.remoting.transport.RpcRequestTransport;
+import com.rpc.remoting.transport.netty.client.NettyRpcClient;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @Author: djc
@@ -12,9 +24,65 @@ import java.lang.reflect.Method;
  */
 @Slf4j
 public class RpcClientProxy implements InvocationHandler {
-    // TODO: 2024/9/12  
+
+    private static final String INTERFACE_NAME = "interfaceName";
+
+    private final RpcRequestTransport rpcRequestTransport;
+    private final RpcServiceConfig rpcServiceConfig;
+
+    public RpcClientProxy(RpcRequestTransport rpcRequestTransport, RpcServiceConfig rpcServiceConfig) {
+        this.rpcRequestTransport = rpcRequestTransport;
+        this.rpcServiceConfig = rpcServiceConfig;
+    }
+
+    public RpcClientProxy(RpcRequestTransport rpcRequestTransport) {
+        this.rpcRequestTransport = rpcRequestTransport;
+        this.rpcServiceConfig = new RpcServiceConfig();
+    }
+
+    /**
+     * 获取代理对象
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T getProxy(Class<T> clazz) {
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, this);
+    }
+
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+    public Object invoke(Object proxy, Method method, Object[] args) {
+        log.info("invoked method: [{}]", method.getName());
+        RpcRequest rpcRequest = RpcRequest.builder().methodName(method.getName())
+                .parameters(args)
+                .interfaceName(method.getDeclaringClass().getName())
+                .paramTypes(method.getParameterTypes())
+                .requestId(UUID.randomUUID().toString())
+                .version(rpcServiceConfig.getVersion())
+                .group(rpcServiceConfig.getGroup())
+                .build();
+        RpcResponse<Object> rpcResponse=null;
+        if(rpcRequestTransport instanceof NettyRpcClient){
+            CompletableFuture<RpcResponse<Object>> completableFuture = (CompletableFuture<RpcResponse<Object>>) rpcRequestTransport.sendRpcRequest(rpcRequest);
+            rpcResponse = completableFuture.get();
+        }
+        this.check(rpcResponse,rpcRequest);
+        return rpcResponse.getData();
+    }
+    private void check(RpcResponse<Object> rpcResponse,RpcRequest rpcRequest){
+        if(rpcResponse==null){
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_INVOCATION_FAILURE, INTERFACE_NAME + ":" + rpcRequest.getInterfaceName());
+        }
+        if (!rpcRequest.getRequestId().equals(rpcResponse.getRequestId())) {
+            throw new RpcException(RpcErrorMessageEnum.REQUEST_NOT_MATCH_RESPONSE, INTERFACE_NAME + ":" + rpcRequest.getInterfaceName());
+        }
+
+        if (rpcResponse.getCode() == null || !rpcResponse.getCode().equals(RpcResponseCodeEnum.SUCCESS.getCode())) {
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_INVOCATION_FAILURE, INTERFACE_NAME + ":" + rpcRequest.getInterfaceName());
+        }
     }
 }
